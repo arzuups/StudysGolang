7551db2e67094a09305c1dba2def94ebe4062fbd
Arzu Demir <119745513+theArzuw@users.noreply.github.com>
2023-03-22 18:44:23 +0300

    Create question1.go

---
+++ Ubn-Homeworks/question1.go
@@ -0,0 +1,22 @@
+/*Find the sum of the two defined numbers.
+1-)Create two variables n, m and assign values to them.
+2) Create a variable called sum.
+3) Add the variables n and m and save the result in the variable sum.
+4-)Print the result.*/
+
+package main
+
+import "fmt"
+
+func main() {
+
+	var num1, num2 int
+	fmt.Println("Enter the First Number :=  ")
+	fmt.Scanln(&num1)
+
+	fmt.Println("Enter the Second Number :=  ")
+	fmt.Scanln(&num2)
+
+	fmt.Println("The Sum of num1 and num2  :=  ", num1+num2)
+
+}
