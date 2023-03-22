81172d3bd6292cacb9e29b635b2079ab1fa1f2ae
Arzu Demir <119745513+theArzuw@users.noreply.github.com>
2023-03-22 18:45:29 +0300

    Create question2.go

---
+++ Ubn-Homeworks/question2.go
@@ -0,0 +1,21 @@
+/*Prepare the code and flowchart that prints whether a number is odd or even on the screen.*/
+
+package main
+
+import "fmt"
+
+func main() {
+	//Get number from user
+	fmt.Println("Please enter a number:")
+	var number int
+	fmt.Scanln(&number)
+
+	//Check if the entered number is even
+	if number%2 == 0 {
+		fmt.Println(number, "çift")
+	} else {
+		fmt.Println(number, "tek")
+
+	}
+
+}
