d47646e591f4a0602b1e6010474b46f092ebf9d5
Arzu Demir <119745513+theArzuw@users.noreply.github.com>
2023-03-22 18:48:27 +0300

    Create question3.go

---
+++ Ubn-Homeworks/question3.go
@@ -0,0 +1,17 @@
+/*Prepare the ko and flowchart that prints the sum of numbers from 1 to 100 on the screen.*/
+
+package main
+
+import "fmt"
+
+func main() {
+	//Calculate the sum of wings from 1 to 100
+
+	sum := 0
+	for i := 1; i <= 100; i++ {
+		sum += i
+	}
+
+	//Print the sum to the screen
+	fmt.Println("Sum of numbers between 1 and 100: ", sum)
+}
