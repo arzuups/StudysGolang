de0476081e6f23756a91a9f05581c102bcb5b27a
Arzu Demir <119745513+theArzuw@users.noreply.github.com>
2023-03-22 18:58:27 +0300

    Create question9.go

---
+++ Ubn-Homeworks/question9.go
@@ -0,0 +1,22 @@
+/*Prepare the code and flowchart that sorts the items in a user-generated array in ascending order.*/
+
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{29, 8, 5, 15, 32, 18, 1, 40, 63, 34}
+
+	for i := 0; i < len(arr); i++ {
+
+		for j := i; j < len(arr); j++ {
+
+			if arr[j] < arr[i] {
+
+				arr[i], arr[j] = arr[j], arr[i]
+			}
+		}
+	}
+	fmt.Println(arr)
+
+}
