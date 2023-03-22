e5adc437efd357cb5df0dfd0c8c3d38f0a624d40
Arzu Demir <119745513+theArzuw@users.noreply.github.com>
2023-03-22 18:51:54 +0300

    Create question6.go

---
+++ Ubn-Homeworks/question6.go
@@ -0,0 +1,22 @@
+/*Prepare the code and flowchart that finds the average of an array.*/
+
+package main
+
+import "fmt"
+
+func main() {
+	//Example series
+	numbers := []int{12, 24, 36, 48, 64}
+
+	//Calculate the total
+	var sum int
+	for _, number := range numbers {
+		sum += number
+	}
+
+	//Calculate average
+	average := float64(sum) / float64(len(numbers))
+
+	//Print the result
+	fmt.Println("Average: ", average)
+}
