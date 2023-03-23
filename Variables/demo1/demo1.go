cae3ea3eb3a6c5a445bc303d1ac3a576b855cbdf
Arzu Demir <119745513+theArzuw@users.noreply.github.com>
2023-03-23 06:15:21 +0300

    Update demo1.go

--- Variables/demo1/demo1.go
+++ Variables/demo1/demo1.go
@@ -1,5 +1,35 @@
+/*You need to run main.go to print to the terminal.*/
 package variables
 
+import (
+	"fmt"
+)
+
 func Demo1() {
+
+	//The "string" function is used for textual data.
+	var text string = "Hello World..."
+	    fmt.Print(text)
+	    fmt.Print(text)
+	    fmt.Print(text)
+	    fmt.Println(text)
+
+	//The "int" data type is used for natural numbers.
+	var age int = 20
+	    fmt.Println(age)
+
+	//The data types "float32" or "float64" are used for decimal numbers.
+	var number float32 = 25.3
+	    fmt.Println(number)
+
+	//Represents true or false. If you do not specify any value, the default is zero.
+	var stuatio bool
+
+	var text1 string = "Olivia"
+	var text2 string = "Michale"
+
+	stuatio = text1 == text2
+
+	    fmt.Println(stuatio)
 
 }
