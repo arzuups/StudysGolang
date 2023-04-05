7aac22a5bf84e2c1a82007e247f5e1fd8b640ea3
Arzu Demir <119745513+a1zuws@users.noreply.github.com>
2023-04-06 00:31:33 +0300

    Update demo2.go

--- Conditionals/demo2/demo2.go
+++ Conditionals/demo2/demo2.go
@@ -1,1 +1,18 @@
+package conditionals
 
+import (
+	"fmt"
+)
+
+func Demo2() {
+	var account float64 = 2000
+	var requested float64 = 400
+
+	if requested > account {
+		fmt.Println("Not enough money in the account.")
+	} else if requested == account {
+		fmt.Println("Your money is being prepared...")
+	} else {
+		fmt.Println("Your money is being prepared...")
+	}
+}
