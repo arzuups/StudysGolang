3e588581c881034bb14c0f0bb39e86d8779147bd
Arzu Demir <119745513+a1zuws@users.noreply.github.com>
2023-04-06 00:02:24 +0300

    Update demo1.go

--- Conditionals/demo1/demo1.go
+++ Conditionals/demo1/demo1.go
@@ -1,1 +1,22 @@
+package conditionals
 
+import (
+	"fmt"
+)
+
+func Demo1() {
+	var account float64 = 2000
+	var requested float64 = 400
+
+	if requested > account {
+		fmt.Println("Not enough money in the account.")
+	}
+
+	if requested <= account {
+		fmt.Println("Your money is being prepared...")
+		account = account - requested
+	}
+
+	fmt.Println("Finish! Money in the account : " + fmt.Sprintf("%v", account))
+	//fmt.Printf("Finish! Money in the account : %v", account) --> fmt.Printf is written as
+}
