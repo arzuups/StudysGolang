2a85ddb08417f06b8834b1c3f3b4eed8733aae58
Arzu Demir <119745513+a1zuws@users.noreply.github.com>
2023-04-06 02:04:18 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	"goStudys/conditionals"
+	"goStudys/loops"
 )
 
 func main() {
 	//variables.Demo1()
+
 	//conditionals.Demo1()
 	//conditionals.Demo2()
-	conditionals.Workshop1()
+	//conditionals.Workshop1()
+
+	loops.Demo1()
 }
