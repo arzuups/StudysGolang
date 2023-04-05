16c2dc2fd411b2ecb11de7ee58988def96777e7c
Arzu Demir <119745513+a1zuws@users.noreply.github.com>
2023-04-06 00:54:36 +0300

    Update workshop1.go

--- Conditionals/workshop1/workshop1.go
+++ Conditionals/workshop1/workshop1.go
@@ -1,1 +1,18 @@
+package conditionals
 
+import "fmt"
+
+func Workshop1() {
+	var num1, num2, num3 int = 12, 45, 23
+
+	var max int = num1
+
+	if num2 > max {
+		max = num2
+	}
+
+	if num3 > max {
+		max = num3
+	}
+	fmt.Printf("Max number : %d", max)
+}
