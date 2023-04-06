410ebcbf24208d7719d7c6e84c695af2617410f7
Arzu Demir <119745513+a1zuws@users.noreply.github.com>
2023-04-06 20:46:09 +0300

    Update demo1.go

--- Loops/demo1/demo1.go
+++ Loops/demo1/demo1.go
@@ -1,5 +1,20 @@
 package loops
 
+import (
+  "fmt"
+)
+
 func Demo1() {
+
+	var text string = "Hello World...Welcome the Go Programming Language..."
+
+	i := 1
+
+	for i <= 100 {
+		fmt.Println(text)
+		i = i + 1
+	}
+
+	fmt.Println("Finish!")
 
 }
