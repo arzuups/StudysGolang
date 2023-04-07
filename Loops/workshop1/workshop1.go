1960c9bfbabc35b6d9497037fc26fae912bd7c3d
Arzu Demir <119745513+a1zuws@users.noreply.github.com>
2023-04-07 05:38:55 +0300

    Update workshop1.go

--- Loops/workshop1/workshop1.go
+++ Loops/workshop1/workshop1.go
@@ -1,28 +1,43 @@
 package loops
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func Workshop1() {
 
 	myNumber := 30
 	estimatedNumber := 0
+	forecastNumber := 0
 
 	fmt.Println("Please give me a number:")
 	fmt.Scanln(&estimatedNumber) //90
+	forecastNumber = forecastNumber + 1
 
 	for estimatedNumber != myNumber {
 		if estimatedNumber < myNumber {
 			fmt.Println("Enter a larger number:")
 			fmt.Scanln(&estimatedNumber)
+			forecastNumber = forecastNumber + 1
 		}
 
 		if estimatedNumber > myNumber {
 			fmt.Println("Enter the smaller number:")
-			fmt.Scanln(&estimatedNumber) 
+			fmt.Scanln(&estimatedNumber)
+			forecastNumber = forecastNumber + 1
 		}
 	}
 
-	if estimatedNumber == myNumber {
-		fmt.Println("You got it right!!!")
+	successStatus := ""
+	if forecastNumber > 0 && forecastNumber <= 3 {
+		successStatus = "Super"
+	} else if forecastNumber <= 6 {
+		successStatus = "Beautiful"
+	} else {
+		successStatus = "Not bad"
 	}
+
+	fmt.Printf("You got it right!!! %v estimated : %v", forecastNumber, successStatus)
+
 }
+
