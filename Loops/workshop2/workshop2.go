ec6a96a9783d376e590b9c3e171bc287b4e4438b
Arzu Demir <119745513+a1zuws@users.noreply.github.com>
2023-04-08 00:40:57 +0300

    Update workshop2.go

--- Loops/workshop2/workshop2.go
+++ Loops/workshop2/workshop2.go
@@ -1,20 +1,22 @@
 package loops
 
-import (
-  "fmt"
-)
-  
-func Workshop2 () {
-number := 0
+import "fmt"
+
+func Workshop2() {
+	number := 0
 	fmt.Println("Please enter a number:")
 	fmt.Scanln(&number)
-  
-  prime := true
+
+	prime := true
 	for i := 2; i < number; i++ {
 		if number%i == 0 {
 			prime = false
-			
-			
 		}
+	}
+
+	if prime == true {
+		fmt.Println("This is a prime number.")
+	} else {
+		fmt.Println("This is not a prime number!")
 	}
 }
