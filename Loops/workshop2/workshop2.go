c8024c7c3c230c9dd05a520f8fd109262e5fefe0
Arzu Demir <119745513+a1zuws@users.noreply.github.com>
2023-04-08 00:39:49 +0300

    Update workshop2.go

--- Loops/workshop2/workshop2.go
+++ Loops/workshop2/workshop2.go
@@ -5,5 +5,14 @@ import (
 )
   
 func Workshop2 () {
-
+number := 0
+	fmt.Println("Please enter a number:")
+	fmt.Scanln(&number)
+  
+  prime := true
+	for i := 2; i < number; i++ {
+		if number%i == 0 {
+			prime = false
+		}
+	}
 }
