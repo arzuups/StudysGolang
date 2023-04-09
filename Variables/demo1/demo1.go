6af142111afc1c8a1e6a815f7bc856915cf52e55
Arzu Demir <119745513+a1zuws@users.noreply.github.com>
2023-04-09 05:28:33 +0300

    Update demo1.go

--- Variables/demo1/demo1.go
+++ Variables/demo1/demo1.go
@@ -10,7 +10,7 @@ func Demo1() {
 	//The "string" function is used for textual data.
 	//"10", "Olivia", "Harvard University" etc..
         //The value "10" of type int and the value "10" of type string are never the same value.
-        //variablename := value -> 3rd variable definition method
+        //variablename := value -> 1st variable definition method
         //var test string = "Test"
 	var text string = "Hello World..."
 	    fmt.Print(text)
@@ -20,15 +20,19 @@ func Demo1() {
 
 	//The "int" data type is used for natural numbers.
 	//-5,-3,10,0,23456 etc.
-        //var variablename variabletype = value -> 2nd variable definition method
+        
 	var age int = 20
 	    fmt.Println(age)
 
 	//The data types "float32" or "float64" are used for decimal numbers.
+	//float32 data type can hold fractional numbers.
+        //102.5, -43.567 vs.
+	
 	var number float32 = 25.3
 	    fmt.Println(number)
 
 	//Represents true or false. If you do not specify any value, the default is zero.
+	
 	var stuatio bool
 
 	var text1 string = "Olivia"
