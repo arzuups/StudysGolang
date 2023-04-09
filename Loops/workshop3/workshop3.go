86b26267cb51e38fc335b7082da41b531f3c21cd
Arzu Demir <119745513+a1zuws@users.noreply.github.com>
2023-04-09 06:19:57 +0300

    Update workshop3.go

--- Loops/workshop3/workshop3.go
+++ Loops/workshop3/workshop3.go
@@ -1,5 +1,37 @@
 package loops
 
 func Workshop3() {
-  
+  package loops
+
+import (
+	"fmt"
+)
+
+func Workshop3() {
+	number1 := 220
+	number2 := 284
+	sum1 := 0
+	sum2 := 0
+
+	for i := 1; i < number1; i++ {
+		if number1%i == 0 {
+			sum1 = sum1 + i
+		}
+	}
+
+	for i := 1; i < number2; i++ {
+		if number2%i == 0 {
+			sum2 = sum2 + i
+
+		}
+
+	}
+	if sum1 == number2 && sum2 == number1 {
+		fmt.Printf("%v and %v are friend numbers.", number1, number2)
+	} else {
+		fmt.Printf("%v and %v are not friendly numbers.", number1, number2)
+	}
+
+}
+
 }
