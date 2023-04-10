c9240086931f85cf01692e549146ef1a58fc7619
Arzu Demir <119745513+a1zuws@users.noreply.github.com>
2023-04-10 23:22:21 +0300

    Update demo2.go

--- arrays/demo2/demo2.go
+++ arrays/demo2/demo2.go
@@ -1,1 +1,21 @@
 
+package arrays
+
+import "fmt"
+
+func Demo2() {
+
+	var cities [5]string
+	cities[0] = "Moscow"
+	cities[1] = "London"
+	cities[2] = "Sankt-Peterburg"
+	cities[3] = "Berlin"
+	cities[4] = "Prague"
+
+	fmt.Println(cities) // This is how you print side by side.
+
+	for i := 0; i < 5; i++ {
+		fmt.Println(cities[i]) // This is how it is written, one below the other.
+	}
+
+}
