6d8cddd4ca1619c47b19af82461885637c65518b
Arzu Demir <119745513+a1zuws@users.noreply.github.com>
2023-04-11 06:01:31 +0300

    Update demo4.go

--- arrays/demo4/demo4.go
+++ arrays/demo4/demo4.go
@@ -1,1 +1,27 @@
+package arrays
+
+import "fmt"
+
+func Demo4() {
+
+	var numbers [2][3]int
+
+	numbers[0][0] = 1
+	numbers[0][1] = 3
+	numbers[0][2] = 5
+	numbers[1][0] = 2
+	numbers[1][1] = 4
+	numbers[1][2] = 6
+
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+			fmt.Print(numbers[i][j])
+			fmt.Print(" ")
+		}
+		fmt.Println()
+	}
+
+	//fmt.Println(numbers[1][1])
+
+}
 
