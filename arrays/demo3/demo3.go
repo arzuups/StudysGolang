31fc05861d9e13a273dab3fb082098fe38e49484
Arzu Demir <119745513+a1zuws@users.noreply.github.com>
2023-04-11 05:29:23 +0300

    Update demo3.go

--- arrays/demo3/demo3.go
+++ arrays/demo3/demo3.go
@@ -1,1 +1,21 @@
+package arrays
+
+import "fmt"
+
+func Demo3() {
+
+	numbers := [5]int{13, 54, 23, 64, 87}
+	fmt.Println(numbers)
+
+	biggest := numbers[0]
+
+	//LENGTH = UZUNLUK
+	for i := 0; i < len(numbers); i++ {
+		if numbers[i] > biggest {
+			biggest = numbers[i]
+
+		}
+	}
+	fmt.Println("Biggest :", biggest)
+}
 
