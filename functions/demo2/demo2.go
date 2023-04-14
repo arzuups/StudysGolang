b6e4047b20bc4ce8c4b07bb0cf32897d21b18c4a
Arzu Demir <119745513+a1zuws@users.noreply.github.com>
2023-04-15 00:45:25 +0300

    Update demo2.go

--- functions/demo2/demo2.go
+++ functions/demo2/demo2.go
@@ -5,5 +5,6 @@ func FourOperations(num1 int, num2 int) (int, int, int, float32) {
   total := num1 + num2
   extraction := num1 - num2
   multiplication := num1 * num2
+  partition := float32(num1 / num2)
   
 }
