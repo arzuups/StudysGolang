c112f0fd2c9e173e5722cfb886c839ac50c1bebe
Arzu Demir <119745513+a1zuws@users.noreply.github.com>
2023-04-16 01:14:43 +0300

    Update main.go

--- main.go
+++ main.go
@@ -27,10 +27,14 @@ func main() {
 	
 	/*var conclusion = functions.Collect(13, 98)
 	fmt.Println(conclusion)*/
+	
 	//functions.Hello(("Don't give up!")
+	
 	/*conclusion1, conclusion2, conclusion3, conclusion4 := functions.FourOperations(34, 98)
 	fmt.Println("Total :", conclusion1)
 	fmt.Println("Extraction :", conclusion2)
 	fmt.Println("Multiplication :", conclusion3)
 	fmt.Println("Partition:", conclusion4)*/
+	
+	functions
 }
