3d8264ff7ca5fab2c019fba7766170d9b01696bb
Arzu Demir <119745513+a1zuws@users.noreply.github.com>
2023-04-17 21:24:19 +0300

    Update demo1.go

--- for_range/demo1/demo1.go
+++ for_range/demo1/demo1.go
@@ -1,1 +1,23 @@
+package for_range
+
+import (
+	"fmt"
+)
+
+func Demo1() {
+
+	cities := []string{"Prague", "Istanbul", "New York"}
+
+	for i := 0; i < len(cities); i++ {
+		fmt.Println(cities[i])
+
+	}
+
+	for i, city := range cities {
+		fmt.Print(i)
+		fmt.Println(city)
+
+	}
+
+}
 
