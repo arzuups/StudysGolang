a591757cd4705fa1db83827b28476df48da8b9cb
Arzu Demir <119745513+a1zuws@users.noreply.github.com>
2023-04-17 21:28:43 +0300

    Update demo2.go

--- for_range/demo2/demo2.go
+++ for_range/demo2/demo2.go
@@ -1,1 +1,21 @@
+package for_range
+
+import (
+	"fmt"
+)
+
+func Demo2() {
+
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	sum := 0
+
+	for _, number := range numbers {
+		if number%2 != 0 {
+			sum = sum + number
+
+		}
+		fmt.Println("Sum :", sum)
+	}
+
+}
 
