e8f3424df8207dcbfdaa774ceeaa6319ffae8438
Arzu Demir <119745513+a1zuws@users.noreply.github.com>
2023-04-17 22:06:16 +0300

    Update demo3.go

--- for_range/demo3/demo3.go
+++ for_range/demo3/demo3.go
@@ -1,5 +1,17 @@
 package for_range
 
+import (
+	"fmt"
+)
+
 func Demo3() {
-  
+
+	dictionary := map[string]string{"Bowl": "Kase", "Plate": "Tabak"}
+
+	for k, v := range dictionary {
+		fmt.Print(k)
+		fmt.Println(v)
+	}
+
 }
+
