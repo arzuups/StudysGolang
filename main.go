d271a6abd39df6b99e270eafdd2ae85b0342d71b
Arzu Demir <119745513+a1zuws@users.noreply.github.com>
2023-04-18 01:51:43 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 func main() {
+
 	//variables.Demo1()
 
 	//conditionals.Demo1()
@@ -12,28 +13,36 @@ func main() {
 	//loops.Workshop1()
 	//loops.Workshop2()
 	//loops.Workshop3()
-	
+
 	//arrays.Demo1()
 	//arrays.Demo2()
 	//arrays.Demo3()
 	//arrays.Demo4()
-	
+
 	//slices.Demo1()
 	//slices.Demo2()
-	
-	/*var conclusion = functions.Collect(13, 98)
+
+	/*var conclusion = functions.Collect(13, 98) //functions.Demo1()
 	fmt.Println(conclusion)*/
-	
-	//functions.Hello(("Don't give up!")
-	
+
+	//functions.Hello(("Don't give up!") //functions.Demo1()
+
 	/*conclusion1, conclusion2, conclusion3, conclusion4 := functions.FourOperations(34, 98)
 	fmt.Println("Total :", conclusion1)
 	fmt.Println("Extraction :", conclusion2)
 	fmt.Println("Multiplication :", conclusion3)
-	fmt.Println("Partition:", conclusion4)*/
-	
+	fmt.Println("Partition:", conclusion4)*/ //functions.Demo2()
+
+	/*fmt.Println(functions.TotalVariadic(3, 8, 7, 9, 6)) // function.Demo3()
+	fmt.Println(functions.TotalVariadic(6, 1, 3))
+	fmt.Println(functions.TotalVariadic())
+	numbers := []int{6, 7, 5, 0}
+	fmt.Println(functions.TotalVariadic(numbers...))*/
+
+	//maps.Demo1()
+	//maps.Demo1("Number of elements:", len(dictionary)) //If we want to print the number of elements
+
 	//for_range.Demo1()
 	//for_range.Demo2()
 	//for_range.Demo3()
-	
 }
