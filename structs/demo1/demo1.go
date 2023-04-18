4c2576a194d0eee19e68b574864ffc1a1aaa421a
Arzu Demir <119745513+a1zuws@users.noreply.github.com>
2023-04-19 01:14:17 +0300

    Update demo1.go

--- structs/demo1/demo1.go
+++ structs/demo1/demo1.go
@@ -6,13 +6,13 @@ import (
 
 func Demo1() {
   fmt.Println(product{"Telephone" , 5.500 , "Iphone"})
-  //fmt.Println(product{name: "Telephone", unitPrice: 5.500, bland: "Iphone"}) //fmt.Println(product{"Telephone", 5.500, "Iphone"})
+  //fmt.Println(product{name: "Telephone", unitPrice: 5.500, brand: "Iphone"}) //If we want to identify them by name
 
 }
 
 type product struct{
   name string
   unitPrice float64
-  bland string
+  brand string
   
 }
