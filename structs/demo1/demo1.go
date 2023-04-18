a3ad1f35e2224b5648c296e227774795a9a57180
Arzu Demir <119745513+a1zuws@users.noreply.github.com>
2023-04-19 01:13:34 +0300

    Update demo1.go

--- structs/demo1/demo1.go
+++ structs/demo1/demo1.go
@@ -6,6 +6,8 @@ import (
 
 func Demo1() {
   fmt.Println(product{"Telephone" , 5.500 , "Iphone"})
+  //fmt.Println(product{name: "Telephone", unitPrice: 5.500, bland: "Iphone"}) //fmt.Println(product{"Telephone", 5.500, "Iphone"})
+
 }
 
 type product struct{
