d59f28ceb6ab7174ee0c06d83cc5b44093dc4453
Arzu Demir <119745513+a1zuws@users.noreply.github.com>
2023-04-20 03:27:31 +0300

    Update demo1.go

--- channels/demo1/demo1.go
+++ channels/demo1/demo1.go
@@ -14,3 +14,13 @@ func EvenNumbers(evenNumberCn chan int) {
 	}
 	evenNumberCn <- sum
 }
+
+func OddNumbers(oddNumberCn chan int) {
+	sum := 0
+	for i := 1; i <= 10; i += 2 {
+		sum = sum + i
+		fmt.Println("Single number addition is working!")
+		time.Sleep(1 * time.Second)
+	}
+	oddNumberCn <- sum
+}
