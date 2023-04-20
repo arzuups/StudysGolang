b649ce229063f40f04433ea088cd1a4c64310450
Arzu Demir <119745513+a1zuws@users.noreply.github.com>
2023-04-20 03:27:11 +0300

    Update demo1.go

--- channels/demo1/demo1.go
+++ channels/demo1/demo1.go
@@ -4,3 +4,13 @@ import (
 	"fmt"
 	"time"
 )
+
+func EvenNumbers(evenNumberCn chan int) {
+	sum := 0
+	for i := 0; i <= 10; i += 2 {
+		sum = sum + i
+		fmt.Println("Even number addition works!")
+		time.Sleep(1 * time.Second)
+	}
+	evenNumberCn <- sum
+}
