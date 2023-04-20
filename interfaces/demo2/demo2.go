bf7d1125f548bb5093872939ec5a9be38efb561f
Arzu Demir <119745513+a1zuws@users.noreply.github.com>
2023-04-21 02:20:41 +0300

    Update demo2.go

--- interfaces/demo2/demo2.go
+++ interfaces/demo2/demo2.go
@@ -15,3 +15,15 @@ type Car struct {
 	carInfo            string
 	rate               float64
 }
+
+type CreditCalculator interface {
+	Calculate() float64
+}
+
+func (m Mortgage) Calculate() float64 {
+	return m.creditPaymentTotal * m.rate / 12
+}
+
+func (c Car) Calculate() float64 {
+	return c.creditPaymentTotal * c.rate / 12
+}
