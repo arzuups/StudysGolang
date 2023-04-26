b21587d06c12ae8d34e78aa36f001f2a86988562
Arzu Demir <119745513+a1zuws@users.noreply.github.com>
2023-04-26 03:05:23 +0300

    Update demo3.go

--- error_handling/demo3/demo3.go
+++ error_handling/demo3/demo3.go
@@ -10,6 +10,5 @@ func predict(predict int) (string, error) {
 	numberInMind := 80
 	
 	if predict < 1 || predict > 100 {
-		return "", errors.New("Enter a number between 1 and 100:")
-
+	    return "", errors.New("Enter a number between 1 and 100:")
 }
