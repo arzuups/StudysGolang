e5a4b6674ce111dac62e349ef5e565dd9210ab24
Arzu Demir <119745513+a1zuws@users.noreply.github.com>
2023-04-29 17:23:33 +0300

    Update demo1.go

--- project/demo1/demo1.go
+++ project/demo1/demo1.go
@@ -1,1 +1,38 @@
+package project
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type Product struct {
+	Id          int     `json:"id"`
+	ProductName string  `json:"productName"`
+	CategoryId  int     `json:"categoryId"`
+	UnitPrice   float64 `json:"unitPrice"`
+}
+
+type Category struct {
+	Id           int    `json:"id"`
+	CategoryName string `json:"categoryName"`
+}
+
+func GetAllProducts() {
+	response, err := http.Get("http://localhost:3000/products")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer response.Body.Close()
+
+	bodyBytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var products []Product
+	json.Unmarshal(bodyBytes, &products)
+	fmt.Println(products)
+}
 
