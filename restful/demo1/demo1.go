edc6bf58b697eba0002cb1344e397b8100cfcab5
Arzu Demir <119745513+a1zuws@users.noreply.github.com>
2023-04-29 14:05:44 +0300

    Update demo1.go

--- restful/demo1/demo1.go
+++ restful/demo1/demo1.go
@@ -1,1 +1,36 @@
 package restful
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type Todo struct {
+	UserId    int    `json:"userId"`
+	Id        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+//GET REQUEST
+func Demo1() {
+	response, err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer response.Body.Close()
+
+	bodyBytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	bodyString := string(bodyBytes)
+	fmt.Println(bodyString)
+
+	var todo Todo
+	json.Unmarshal(bodyBytes, &todo)
+	fmt.Println(todo)
+}
