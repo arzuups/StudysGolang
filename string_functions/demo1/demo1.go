b5eddc617eaaeb725e4ce3620b8689f79ecd7885
Arzu Demir <119745513+a1zuws@users.noreply.github.com>
2023-04-29 10:56:00 +0300

    Update demo1.go

--- string_functions/demo1/demo1.go
+++ string_functions/demo1/demo1.go
@@ -2,26 +2,26 @@ package string_functions
 
 import (
 	"fmt"
-	"strings"
+	s "strings"
 )
 
 func Demo1() {
 	name := "Arzu"
-	fmt.Println(strings.Count(name, "r")) // => 1
-	fmt.Println(strings.Count(name, "z")) // => 1
-	fmt.Println(strings.Count(name, "b")) // => 0
-	fmt.Println(strings.Contains(name, "c")) // => false
-	fmt.Println(strings.Count(name, "A"))    // => 1
-	fmt.Println(strings.Count(name, "u")) // => 1
+	fmt.Println(s.Count(name, "r")) // => 1
+	fmt.Println(s.Count(name, "z")) // => 1
+	fmt.Println(s.Count(name, "b")) // => 0
+	fmt.Println(s.Contains(name, "c")) // => false
+	fmt.Println(s.Count(name, "A"))    // => 1
+	fmt.Println(s.Count(name, "u")) // => 1
 	
-	conclusion := strings.Index(name, "A")
+	conclusion := s.Index(name, "A")
 	
 	if conclusion != -1 {
 		fmt.Println("There's the letter `A`")
 	} else {
 		fmt.Println("No letter `A`")
 	}
-	fmt.Println(strings.ToLower(name)) // => arzu
-	fmt.Println(strings.ToUpper(name)) // => ARZU
+	fmt.Println(s.ToLower(name)) // => arzu
+	fmt.Println(s.ToUpper(name)) // => ARZU
 }
 
