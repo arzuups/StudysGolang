b6f4a74957000e070edb7d747099c35ce99dc4be
Arzu Demir <119745513+a1zuws@users.noreply.github.com>
2023-04-29 14:28:03 +0300

    Update main.go

--- main.go
+++ main.go
@@ -23,40 +23,40 @@ func main() {
 	//slices.Demo2()
 
 	/*var conclusion = functions.Collect(13, 98) //functions.Demo1()
-	fmt.Println(conclusion)*/
+	  fmt.Println(conclusion)*/
 
 	//functions.Hello(("Don't give up!") //functions.Demo1()
 
 	/*conclusion1, conclusion2, conclusion3, conclusion4 := functions.FourOperations(34, 98)
-	fmt.Println("Total :", conclusion1)
-	fmt.Println("Extraction :", conclusion2)
-	fmt.Println("Multiplication :", conclusion3)
-	fmt.Println("Partition:", conclusion4)*/ //functions.Demo2()
+	  fmt.Println("Total :", conclusion1)
+	  fmt.Println("Extraction :", conclusion2)
+	  fmt.Println("Multiplication :", conclusion3)
+	  fmt.Println("Partition:", conclusion4)*/ //functions.Demo2()
 
 	/*fmt.Println(functions.TotalVariadic(3, 8, 7, 9, 6)) // function.Demo3()
-	fmt.Println(functions.TotalVariadic(6, 1, 3))
-	fmt.Println(functions.TotalVariadic())
-	numbers := []int{6, 7, 5, 0}
-	fmt.Println(functions.TotalVariadic(numbers...))*/
+	  fmt.Println(functions.TotalVariadic(6, 1, 3))
+	  fmt.Println(functions.TotalVariadic())
+	  numbers := []int{6, 7, 5, 0}
+	  fmt.Println(functions.TotalVariadic(numbers...))*/
 
 	//maps.Demo1()
 
 	//for_range.Demo1()
 	//for_range.Demo2()
 	//for_range.Demo3()
-	
+
 	/*number := 140
-	pointers.Demo1(&number)
-	fmt.Println("The number in Main:", number)*/
-	
-	//structs.Demo1()
-	//structs.Demo2()
-	
+	  pointers.Demo1(&number)
+	  fmt.Println("The number in Main:", number)*/
+
+	// structs.Demo1()
+	// structs.Demo2()
+
 	/*go goroutines.EvenNumbers()
 	go goroutines.OddNumbers()
 	time.Sleep(10 * time.Second)
 	fmt.Println("Main finished!")*/
-	
+
 	/*evenNumberCn := make(chan int)
 	oddNumberCn := make(chan int)
 	go channels.EvenNumbers(evenNumberCn)
@@ -70,19 +70,19 @@ func main() {
 	//interfaces.Demo1()
 	//interfaces.Demo2()
 	//interfaces.Demo3()
-	
+
 	//defer_statement.A()
 	//defer_statement.TestDemo2()
 	//defer_statement.Demo3()
-	
+
 	//error_handling.Demo1()
 	//error_handling.Demo2()
 	//error_handling.Demo3()
 	//fmt.Println(error_handling.Predict2(140))
-	
+
 	//string_functions.Demo1()
 	//string_functions.Demo2()
-	
+
 	//restful.Demo1()
 	//restful.Demo2()
 }
