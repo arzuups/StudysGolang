7c7e2e04f3dacc184341bf8a54b13be042a81097
Arzu Demir <119745513+a1zuws@users.noreply.github.com>
2023-05-01 13:48:08 +0300

    Update question10.go

--- Ubn-Homeworks/question10/question10.go
+++ Ubn-Homeworks/question10/question10.go
@@ -14,8 +14,10 @@ import (
 func main() {
 	arr := []int{30, 60, 40, 70, 30, 50, 10, 80, 40, 50, 70, 20} // The array is defined
 	freq := make(map[int]int)
-	for _, num := range arr {
+	
+	for _, num := range arr {// Start a loop for each number in the array.
 		freq[num] = freq[num] + 1
 	}
+	
 	fmt.Println("Frequency of the Array is : ", freq)
 }
