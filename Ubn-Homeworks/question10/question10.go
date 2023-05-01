c025cdacb3332e2874ba86c3299f40a74fcb08b0
Arzu Demir <119745513+a1zuws@users.noreply.github.com>
2023-05-01 13:47:09 +0300

    Update question10.go

--- Ubn-Homeworks/question10/question10.go
+++ Ubn-Homeworks/question10/question10.go
@@ -12,7 +12,7 @@ import (
 )
 
 func main() {
-	arr := []int{30, 60, 40, 70, 30, 50, 10, 80, 40, 50, 70, 20}
+	arr := []int{30, 60, 40, 70, 30, 50, 10, 80, 40, 50, 70, 20} // The array is defined
 	freq := make(map[int]int)
 	for _, num := range arr {
 		freq[num] = freq[num] + 1
