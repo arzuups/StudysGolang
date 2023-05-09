726f2804d1e868b1664b6d256fe11da9ca11fde7
Arzu Demir <119745513+a1zuws@users.noreply.github.com>
2023-05-10 00:17:38 +0300

    Update demo2.go

--- functions/demo2/demo2.go
+++ functions/demo2/demo2.go
@@ -1,7 +1,7 @@
 /*You need to run main.go to print to the terminal.*/
 package functions
 
-func FourOperations(num1 int, num2 int) (int, int, int, float32) {  
+func FourOperations(num1 int, num2 int) (int, int, int, float32) {
   total := num1 + num2
   extraction := num1 - num2
   multiplication := num1 * num2
