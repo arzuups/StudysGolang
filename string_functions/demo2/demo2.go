ff211c7a399e4cc0aebe5a24b72c0a19ff3460c1
Arzu Demir <119745513+arzuups@users.noreply.github.com>
2023-07-05 22:30:04 +0300

    Update demo2.go

--- string_functions/demo2/demo2.go
+++ string_functions/demo2/demo2.go
@@ -1,4 +1,6 @@
 /*You need to run main.go to print to the terminal.*/
+
+/*You need to run main.go to print to the terminal.*/
 package string_functions
 
 import (
@@ -10,14 +12,14 @@ func Demo2() {
 	name := "Arzu"
 	fmt.Println(s.HasPrefix(name, "Ar")) // => true
 	fmt.Println(s.HasSuffix(name, "el")) // => false
-	fmt.Println(s.Index(name, "zu"))  // => 2
-	
-	letters := []string{"a", "r", "z", "u", "w", "s"}
-	conclusion := s.Join(letters, "/") // => a/r/z/u/w/s
-	fmt.Println(conclusion) // =>a/r/z/u/w/s
-	
+	fmt.Println(s.Index(name, "zu"))     // => 2
+
+	letters := []string{"a", "r", "z", "u", "u", "p", "s"}
+	conclusion := s.Join(letters, "/") // => a/r/z/u/u/p/s
+	fmt.Println(conclusion)            // =>a/r/z/u/u/p/s
+
 	fmt.Println(s.Replace(conclusion, "*", "+", 3))
-	fmt.Println(s.Split(conclusion, "-")) //=> [a/r/z/u/w/s]
-	fmt.Println(s.Repeat(conclusion, 6)) // => a/r/z/u/w/sa/r/z/u/w/sa/r/z/u/w/sa/r/z/u/w/sa/r/z/u/w/sa/r/z/u/w/s
-	
-} 
+	fmt.Println(s.Split(conclusion, "-")) //=> [a/r/z/u/u/p/s]
+	fmt.Println(s.Repeat(conclusion, 6))  // => a/r/z/u/u/p/sa/r/z/u/u/p/sa/r/z/u/u/p/sa/r/z/u/u/p/sa/r/z/u/u/p/sa/r/z/u/u/p/s
+
+}
