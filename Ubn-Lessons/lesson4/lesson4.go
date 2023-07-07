faac0911f065bd585746e3035097110867f157f2
Arzu Demir <119745513+arzuups@users.noreply.github.com>
2023-07-07 21:09:33 +0300

    Create lesson4.go

---
+++ Ubn-Lessons/lesson4/lesson4.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//OPERATORS
+	//(+ - * / %)
+
+	//(+)
+	num1 := 13
+	num2 := 34
+	sum := num1 + num2
+	fmt.Println(sum)
+
+	//(-)
+	var number1 float32 = 12.54
+	var number2 float32 = 13.43
+	var extraction float32 = number1 - number2
+	fmt.Println(extraction)
+
+	//(*)
+	myNumber1 := 12
+	myNumber2 := 43
+	Multiplication := myNumber1 * myNumber2
+	fmt.Println(Multiplication)
+
+	//(/)
+	var queueNumber1 int = 60
+	var queueNumber2 int = 100
+	var Partition int = queueNumber1 / queueNumber2
+	fmt.Println(Partition)
+}
