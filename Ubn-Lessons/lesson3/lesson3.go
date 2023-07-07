0bbaa610a3bb65fb2a45c184e2fd27165bc0d712
Arzu Demir <119745513+arzuups@users.noreply.github.com>
2023-07-07 21:09:06 +0300

    Create lesson3.go

---
+++ Ubn-Lessons/lesson3/lesson3.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//VARIABLES
+
+	//string = "Hola!"
+	//name := "Arzuups"
+	var name string = "Arzuups"
+	fmt.Println(name)
+
+	//int = 1 2 3 4 5 6 7 8 9
+	//age := 14
+	var age int = 14
+	fmt.Println(age)
+
+	//float32 - float64
+	//lengthTable := 103.54
+	var lengthTable float32 = 101.4
+	fmt.Println(lengthTable)
+
+	//bool => true - false
+	//aktifMi := true
+	var aktifMi bool = false
+	fmt.Println(aktifMi)
+
+}
