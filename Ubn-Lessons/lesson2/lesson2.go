670dd26db4cfe1cacbed3e56a2380d9db1262f20
Arzu Demir <119745513+arzuups@users.noreply.github.com>
2023-07-07 21:08:01 +0300

    Create lesson2.go

---
+++ Ubn-Lessons/lesson2/lesson2.go
@@ -0,0 +1,22 @@
+package main
+
+func main() {
+
+}
+
+// SOLID PRINCIPLE
+func Sum(num1 int, num2 int) int {
+	return num1 + num2
+}
+
+func Extraction(num1 float32, num2 float32) float32 {
+	return num1 - num2
+}
+
+func Multiplication(num1 int, num2 int) int {
+	return num1 * num2
+}
+
+func Partition(num1 float64, num2 float64) float64 {
+	return num1 / num2
+}
