df2b0ced566e9eedf33d31d8e16d617ef5cd262d
Arzu Demir <119745513+arzuups@users.noreply.github.com>
2023-07-18 13:54:23 +0300

    Update lesson6.go

--- Ubn-Lessons/lesson6/lesson6.go
+++ Ubn-Lessons/lesson6/lesson6.go
@@ -4,5 +4,11 @@ package main
 import "fmt"
 
 func main() {
-  
+
+  var cinemaOpenSentence string = "Cinema opening time"
+	var cinemaOpenHour string = "10"
+	var cinemaOpenMinute string = "00"
+	//var cinemaOpenTime string = cinemaOpenSentence + " " + "=" + " " + cinemaOpenHour + ":" + cinemaOpenMinute
+	var cinemaOpenTime string = combineHourMinuteSentence(cinemaOpenSentence, cinemaOpenHour, cinemaOpenMinute)
+	fmt.Println(cinemaOpenTime)
 }
