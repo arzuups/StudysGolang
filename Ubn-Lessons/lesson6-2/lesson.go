2a2cbfd5b53cb357ff9a8b3c9d2772d8df8252dc
Arzu Demir <119745513+arzuups@users.noreply.github.com>
2023-07-18 16:27:26 +0300

    Update lesson.go

--- Ubn-Lessons/lesson6-2/lesson.go
+++ Ubn-Lessons/lesson6-2/lesson.go
@@ -1,1 +1,44 @@
+// Golang Basic Lesson 6-2
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	day := "25"
+	month := "08"
+	year := "2008"
+	hour := "06"
+	minute := "30"
+
+	conclusion := combineDate(day, month, year, hour, minute)
+	fmt.Println(conclusion)
+}
+
+func combineDate(day string, month string, year string, hour string, minute string) string {
+
+	date := combineDayMonthYear(day, month, year)
+	fmt.Println(date)
+	time := combineHourMinute(hour, minute)
+	fmt.Println(time)
+	dateTime := dateTime(date, time)
+	return dateTime
+}
+
+func combineHourMinute(hour string, minute string) string {
+	time := hour + ":" + minute
+	return time
+}
+
+func combineDayMonthYear(day string, month string, year string) string {
+	date := day + "/" + month + "/" + year
+	return date
+}
+
+func dateTime(date string, time string) string {
+	dateTime := date + " " + time
+	return dateTime
+}
 
