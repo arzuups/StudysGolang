ccea27fce99e0820decaf1873412421b673acae4
Arzu Demir <119745513+arzuups@users.noreply.github.com>
2023-07-18 13:56:44 +0300

    Update lesson6.go

--- Ubn-Lessons/lesson6/lesson6.go
+++ Ubn-Lessons/lesson6/lesson6.go
@@ -1,35 +1,37 @@
-//Golang Basic Lesson-6
+// Golang Basic Lesson-6
 package main
 
-import "fmt"
+import (
+    "fmt"
+)
 
 func main() {
 
-  var cinemaOpenSentence string = "Cinema opening time"
-	var cinemaOpenHour string = "10"
-	var cinemaOpenMinute string = "00"
-	//var cinemaOpenTime string = cinemaOpenSentence + " " + "=" + " " + cinemaOpenHour + ":" + cinemaOpenMinute
-	var cinemaOpenTime string = combineHourMinuteSentence(cinemaOpenSentence, cinemaOpenHour, cinemaOpenMinute)
-	fmt.Println(cinemaOpenTime)
+    var cinemaOpenSentence string = "Cinema opening time"
+    var cinemaOpenHour string = "10"
+    var cinemaOpenMinute string = "00"
+    //var cinemaOpenTime string = cinemaOpenSentence + " " + "=" + " " + cinemaOpenHour + ":" + cinemaOpenMinute
+    var cinemaOpenTime string = combineHourMinuteSentence(cinemaOpenSentence, cinemaOpenHour, cinemaOpenMinute)
+    fmt.Println(cinemaOpenTime)
 
-  var movieStartSentence string = "Movie start time"
-	var movieStartHour string = "12"
-	var movieStartMinute string = "30"
-	//var movieStartTime string = movieStartSentence + " " + "=" + " " + MovieStartHour + ":" + movieStartMinute
-	var movieStartTime string = combineHourMinuteSentence(movieStartSentence, movieStartHour, movieStartMinute)
-	fmt.Println(movieStartTime)
+    var movieStartSentence string = "Movie start time"
+    var movieStartHour string = "12"
+    var movieStartMinute string = "30"
+    //var movieStartTime string = movieStartSentence + " " + "=" + " " + MovieStartHour + ":" + movieStartMinute
+    var movieStartTime string = combineHourMinuteSentence(movieStartSentence, movieStartHour, movieStartMinute)
+    fmt.Println(movieStartTime)
 
- var goSentence string = "My time to go"
-	var goHour string = "12"
-	var goMinute string = "00"
-	//var goTime string = goSentence + " " + "=" + " " + goHour + ":" + goMinute
-	var goTime string = combineHourMinuteSentence(goSentence, goHour, goMinute)
-	fmt.Println(goTime)
+    var goSentence string = "My time to go"
+    var goHour string = "12"
+    var goMinute string = "00"
+    //var goTime string = goSentence + " " + "=" + " " + goHour + ":" + goMinute
+    var goTime string = combineHourMinuteSentence(goSentence, goHour, goMinute)
+    fmt.Println(goTime)
 
 }
 
 func combineHourMinuteSentence(sentence string, hour string, minute string) string {
-	time := sentence + " " + "=" + " " + hour + ":" + minute
-	fmt.Println(time)
-	return time
+    time := sentence + " " + "=" + " " + hour + ":" + minute
+    fmt.Println(time)
+    return time
 }
