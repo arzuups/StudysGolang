327507fa2f69b435e79e4d3505a1730f51e9e658
Arzu Demir <119745513+arzuups@users.noreply.github.com>
2023-07-22 15:11:48 +0300

    Update lesson8.go

--- Ubn-Lessons/lesson8/lesson8.go
+++ Ubn-Lessons/lesson8/lesson8.go
@@ -1,1 +1,34 @@
 // Golang Basic Lesson-8
+package main
+
+import "fmt"
+
+func main() {
+
+	// && => And => Ve
+	// || => Or => Veya
+	// true && true = true
+	// false || false = false
+
+	englishGrade := 90
+	mathGrade := 70
+	passingGrade := 50
+
+	if englishGrade > passingGrade && mathGrade > passingGrade {
+		fmt.Println("You are successful:)")
+	} else {
+		fmt.Println("Unfortunately, you have to work harder:(")
+	}
+
+	var weight float32 = 108.90
+	var training int = 20
+
+	if (weight > 100 && training > 10) ||
+		(weight < 100 && training < 8) {
+		fmt.Println("Congrats!!")
+	} else {
+		fmt.Println("Sorry, result failed...")
+	}
+
+}
+
