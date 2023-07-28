4863b35c4c2d52249e34422d2fa81d10d2d8bbf2
Arzu Demir <119745513+arzuups@users.noreply.github.com>
2023-07-28 15:13:35 +0300

    Update lesson11.go

--- Ubn-Lessons/lesson11/lesson11.go
+++ Ubn-Lessons/lesson11/lesson11.go
@@ -1,1 +1,52 @@
 // Golang Basic Lesson-11
+package main
+
+import "fmt"
+
+func main() {
+
+	// LOOPS
+	/*Continue => Proceeds to the next step.
+	  Break => Get out of the loop.*/
+	var firstNumber int = 0
+	var numbers [8]int = [8]int{4, 6, 8, 7, 3, 2, 9, 1}
+
+	for i := 0; i < 8; i++ {
+		firstNumber = numbers[i]
+		firstNumber *= firstNumber
+		fmt.Println(firstNumber)
+	}
+
+	var randomNumbers [25]int = [25]int{8, 43, 56, 786, 34, 23, 45, 67, 89, 12, 134, 56, 178, 90, 23, 145, 57, 89, 12, 34, 56, 78, 90, 23, 45}
+	var oddNumberQuantity int = 0
+	var evenNumberQuantity int = 0
+	var isİtAValidSeries bool = true
+
+	for i := 0; i < len(randomNumbers); i++ {
+
+		var nextNumber int = randomNumbers[i]
+		fmt.Println(nextNumber)
+		if nextNumber == 0 {
+			isİtAValidSeries = false
+			break
+		}
+		if nextNumber > 99 {
+			continue
+		}
+
+		if nextNumber%2 == 0 {
+			evenNumberQuantity++
+		} else {
+			oddNumberQuantity++
+		}
+	}
+
+	if isİtAValidSeries {
+		fmt.Println("Odd Number Quantity: ", oddNumberQuantity)
+		fmt.Println("Even Number Quantity: ", evenNumberQuantity)
+	} else {
+		fmt.Println("Array invalid...")
+	}
+
+	fmt.Println("Finish.")
+}
